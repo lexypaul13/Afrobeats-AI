72fc11e250d50405e07745fe70c216bc23f6f663
Mayowa Paul <lexypaul13@yahoo.com>
2024-06-09 00:41:16 -0400

    Initial Commit

---
+++ Afrobeats AI/Afrobeats_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Afrobeats_AIApp.swift
+//  Afrobeats AI
+//
+//  Created by Alex Paul on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Afrobeats_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
